7e6dfb5b23be8d7c37a8056b2f3da05254e0c51d
marcoce005 <marcoce005@gmail.com>
2026-05-24 20:25:54 +0200

    update in poli: init lab04

---
+++ poli/labs/lab04/streamlit_lab04/0_Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(
+    page_title="Lab. 04",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://dbdmg.polito.it/",
+        "Report a bug": "https://dbdmg.polito.it/",
+        "About": "# Corso di *Basi di Dati*",
+    },
+)
+
+
+st.title("📈 La mia App")
